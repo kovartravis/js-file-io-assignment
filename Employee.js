95ae061d8db34dcc87706cf12b6b637a265c7d6d
unknown <kovartravis@gmail.com>
2017-10-30 16:30:29 -0500

    first commit

--- Employee.js
+++ Employee.js
@@ -1,16 +1,36 @@
 'use strict'
 
 const fs = require('fs')
+const {resolve} = require('path')
 
 class Employee {
-  constructor (/* ??? */) {
-    // TODO
+  constructor (name, title, salary) {
+    this._name = name
+    this._title = title
+    this._salary = salary
   }
 
-  // TODO ???
-}
+  set title(title){
+    this._title = title
+  }
+  set salary(salary){
+    this._salary = salary
+  }
+  get salary(){
+    return this._salary
+  }
 
-// TODO ???
+
+  static parseFromFilePath(filepath){
+    let input = JSON.parse(fs.readFileSync(filepath))
+    return new Employee(input.name, input.title, input.salary)
+  }
+
+  promote(title, raise){
+    this.title = title
+    this.salary = this.salary + raise
+  }
+}
 
 module.exports = {
   Employee
